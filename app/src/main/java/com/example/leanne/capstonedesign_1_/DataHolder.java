1b9e19a8abb93abb0a4e476181f041b0042efa43
Leanne Lim <limlean590@naver.com>
2016-04-11 12:46:43 +0900

    leanne commit - "추가정보" 기능 구현 (디비 저장 빼고)

--- app/src/main/java/com/example/leanne/capstonedesign_1_/DataHolder.java
+++ app/src/main/java/com/example/leanne/capstonedesign_1_/DataHolder.java
@@ -5,18 +5,28 @@ package com.example.leanne.capstonedesign_1_;
  */
 public class DataHolder {
 
-    public DataHolder(){
+    public DataHolder() {
         //skipSplash = false;
         skipLure = false;
     }
+
     //private boolean skipSplash;
     private boolean skipLure;
+
     //public void setSkipSplash(boolean skipSplash){ this.skipSplash = skipSplash; }
     //public boolean getSkipSplash(){ return skipSplash; }
-    public void setSkipLure(boolean skipLure){ this.skipLure = skipLure; }
-    public boolean getSkipLure(){ return skipLure; }
+    public void setSkipLure(boolean skipLure) {
+        this.skipLure = skipLure;
+    }
+
+    public boolean getSkipLure() {
+        return skipLure;
+    }
 
     // ***
     private static final DataHolder holder = new DataHolder();
-    public static DataHolder getInstance() {return holder;}
+
+    public static DataHolder getInstance() {
+        return holder;
+    }
 }
--- app/src/main/java/com/example/leanne/capstonedesign_1_/ExtraInfoActivity.java
+++ app/src/main/java/com/example/leanne/capstonedesign_1_/ExtraInfoActivity.java
@@ -1,15 +1,40 @@
 package com.example.leanne.capstonedesign_1_;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by imsuyeon on 16. 4. 4..
  */
-public class ExtraInfoActivity extends Activity {
+public class ExtraInfoActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_extra_info);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.button_skip:
+                Intent intentHome = new Intent(this, HomeActivity.class);
+                startActivity(intentHome);
+                this.overridePendingTransition(R.anim.animation_enter_right2left,
+                        R.anim.animation_leave_right2left);
+                this.finish();
+                break;
+            case R.id.button_save:
+                // SAVE "추가정보" to db
+                Toast.makeText(getApplicationContext(), "추가정보 입력 완료", Toast.LENGTH_LONG).show();
+                Intent intentHome2 = new Intent(this, HomeActivity.class);
+                startActivity(intentHome2);
+                this.overridePendingTransition(R.anim.animation_enter_right2left,
+                        R.anim.animation_leave_right2left);
+                this.finish();
+                break;
+        }
     }
 
     @Override
