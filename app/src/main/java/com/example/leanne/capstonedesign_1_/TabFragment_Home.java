81a1a32cb805850c149c927bd2dfe3d901138c0e
kiwi1618 <avocado1618@naver.com>
2016-04-13 12:11:24 +0900

    added settings icon

---
+++ app/src/main/java/com/example/leanne/capstonedesign_1_/TabFragment_Home.java
@@ -0,0 +1,19 @@
+package com.example.leanne.capstonedesign_1_;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Chloe on 4/13/2016.
+ */
+public class TabFragment_Home extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_home, container, false);
+    }
+
+}
